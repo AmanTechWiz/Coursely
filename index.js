8eb6a4589cb489d53f13c6cdcd0ced1176c2628c
Amandeep <eng.aman29@gmail.com>
2025-05-25 17:14:11 +0530

    tst

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const { UserRouter } = require('./routes/users');
 const { CourseRouter } = require('./routes/courses'); 
 const { AdminRouter } = require('./routes/admin');
@@ -8,6 +10,8 @@ app.use('/v1/users',UserRouter);
 app.use('/v1/admin',AdminRouter);
 app.use('/v1/courses',CourseRouter);
 
-app.listen(3000,()=>{
-    console.log("Server Started");
-})
+async function main(){
+    await mongoose.connect('process.env.MONGO_DB');
+   app.listen(3000);
+   console.log('Server started');
+}
